a16ea86b80c481b856988cbd9cfb58f0fc159500
Carlos Brito <cbrito1994@gmail.com>
2021-04-15 13:43:27 -0500

    Created the global variables I'm gonna use

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-let pDay = document.querySelector('.header__date');
+let pDay = $('.header__date');
+let main = $('main');
 let day = moment().format('dddd, MMMM Do');
-pDay.innerHTML = day;
+let timePicker = $('.timePicker');
+let storedActivity = JSON.parse(localStorage.getItem("time"));
+let initialTime = storedActivity !== null ? storedActivity[storedActivity.length - 1].start : 9;
+let finishTime = storedActivity !== null ? storedActivity[storedActivity.length - 1].finish : 18;
+pDay.text(day);
 
