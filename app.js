f728dd6927693bdb37b62433a658b3f77a67e826
Carlos Brito <cbrito1994@gmail.com>
2021-04-15 13:46:31 -0500

    Created the for loop that will allow the user to create the different rows of time depending on the default time set by the code or the customed one

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ let initialTime = storedActivity !== null ? storedActivity[storedActivity.length
 let finishTime = storedActivity !== null ? storedActivity[storedActivity.length - 1].finish : 18;
 pDay.text(day);
 
+for(let i = initialTime; i < finishTime; i++){
+    let rowSchedule = '';
+    rowSchedule += `
+        <form class="rowSchedule">
+            <div class="rowSchedule__time">${i} ${i > 11 ? "PM" : "AM"}</div>
+            <input class="rowSchedule__input" />
+            <button class="rowSchedule__save">
+                <img src="https://image.flaticon.com/icons/png/512/84/84297.png" alt="save" class="inputImage">
+            </button>
+        </form>
+    `
+    main.append(rowSchedule);
+}
